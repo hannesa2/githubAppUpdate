58fa1bbd5974f6feae60f199ac187702f51cc597
Hannes Achleitner <hannes.software@gmx.at>
2021-02-07 17:32:30 +0100

    Optional force

--- githubAppUpdate/src/main/java/info/hannes/github/AppUpdateHelper.kt
+++ githubAppUpdate/src/main/java/info/hannes/github/AppUpdateHelper.kt
@@ -14,10 +14,11 @@ import kotlinx.coroutines.withContext
 import okhttp3.logging.HttpLoggingInterceptor
 
 object AppUpdateHelper {
-    fun checkForNewVersion(activity: AppCompatActivity, gitUser: String, gitRepo: String, versionName: String) = activity.lifecycle.coroutineScope.launch(Dispatchers.Main) {
+    fun checkForNewVersion(activity: AppCompatActivity, gitUser: String, gitRepo: String, versionName: String, force: Boolean = false) =
+            activity.lifecycle.coroutineScope.launch(Dispatchers.Main) {
         val key = "LAST_VERSION_CHECK"
         val prefs = PreferenceManager.getDefaultSharedPreferences(activity)
-        if (prefs.getLong(key, 0) < System.currentTimeMillis() - 1000 * 3600 * 24 / 24 / 60 * 5) {
+        if (force || prefs.getLong(key, 0) < System.currentTimeMillis() - 1000 * 3600 * 24 / 24 / 60 * 5) {
             try {
                 val logEntries = withContext(Dispatchers.Default) {
                     val client = GithubClient(HttpLoggingInterceptor.Level.BODY)
@@ -44,6 +45,7 @@ object AppUpdateHelper {
                                 }
                                 .setPositiveButton(activity.getString(R.string.showRelease)) { dialog, _ ->
                                     val uriUrl = Uri.parse(it.htmlUrl)
+                                    Log.d("open", uriUrl.toString())
                                     activity.startActivity(Intent(Intent.ACTION_VIEW, uriUrl))
                                     dialog.dismiss()
                                 }
@@ -51,6 +53,7 @@ object AppUpdateHelper {
                         assetApk?.let {
                             dialog.setNeutralButton(activity.getString(R.string.directDownload)) { dialog, _ ->
                                 val uriUrl = Uri.parse(it.browserDownloadUrl)
+                                Log.d("open", uriUrl.toString())
                                 activity.startActivity(Intent(Intent.ACTION_VIEW, uriUrl))
                                 dialog.dismiss()
                             }
